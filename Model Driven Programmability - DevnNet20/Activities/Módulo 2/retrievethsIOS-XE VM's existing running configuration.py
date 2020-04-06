d086c74403fd2ecfd1825e5d306b6c7ff19734fb
chunche95 <paeste95.pb@gmail.com>
2020-04-06 21:51:53 +0200

    NETConf Device Configuration

--- Model Driven Programmability - DevnNet20/Activities/Módulo 2/retrievethsIOS-XE VM's existing running configuration.py
+++ Model Driven Programmability - DevnNet20/Activities/Módulo 2/retrievethsIOS-XE VM's existing running configuration.py
@@ -4,12 +4,17 @@ os.system("clear")
 os.system("cls")
 
 from ncclient import manager
-manager = manager.connect(
+
+with manager.connect(
     host="192.168.56.101",
     port=830,
     username="cisco",
     password="cisco123!",
     hostkey_verify=False
-)
+)as m:
+    c = m.get_config(source='running').data_xml
+    with open("%s.xml" % host, 'w') as f:
+        f.write(c)
+        
 netconf_reply  = manager.get_config(source="running")
-print(netconf_reply)
+print(netconf_reply) 
