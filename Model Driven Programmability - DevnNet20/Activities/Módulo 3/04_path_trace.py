3ae1d42baa9b35e2fe97d417e9ef83dda0d90334
chunche95 <paeste95.pb@gmail.com>
2020-04-08 16:54:42 +0200

    Ya empezamos mal...

---
+++ Model Driven Programmability - DevnNet20/Activities/Módulo 3/04_path_trace.py
@@ -0,0 +1,48 @@
+#!/bin/python
+import os
+os.system("clear")
+os.system("cls")
+
+# Import the libraries
+import json
+import requests
+import tabulate
+
+
+from tabulate import *
+from my_apic_em_functions import *
+
+# Disable the warnings 
+requests.packages.urllib3.disable_warnings()
+
+
+
+# Built the request components
+api_url = "https://sandboxapicem.cisco.com/api/v1/flow-analysis"
+headers = {
+    "content-type": "application/json"
+}
+body_json = {
+    "username": "devnetuser",
+    "password": "Cisco123!"
+}
+
+# Send the request
+resp=requests.post(api_url,json.dumps(body_json),headers=headers,verify=False)
+
+# Create the header dictionary
+headers = []
+
+i = 0
+for item in response_json["response"]:
+     i+=1
+     host = [
+             i,
+             item["hostType"],
+             item["hostIp"] 
+            ]
+     headers.append( host )
+table_header = ["Number", "Type", "IP"]
+print( tabulate(headers, table_header) )
+print("string")
+function_name()
---
+++ Model Driven Programmability - DevnNet20/Activities/Módulo 3/print_devices.py
@@ -0,0 +1,45 @@
+#!/bin/python
+import os
+os.system("clear")
+os.system("cls")
+
+import requests
+import json
+import tabulate
+
+
+from tabulate import *
+from my_apic_em_functions import *
+
+# Built the request components
+api_url = "https://sandboxapicem.cisco.com/api/v1/host"
+ticket = get_ticket()
+headers = {
+ "content-type": "application/json",
+ "X-Auth-Token": ticket
+}
+# Request and handle errors.
+resp = requests.get(api_url, headers=headers, verify=False)
+print("Status of /host request: ", resp.status_code)
+if resp.status_code != 200:
+    raise Exception("Status code does not equal 200. Response text: " + resp.text)
+response_json = resp.json()
+
+
+# Parse and format the JSON response data
+# Create a new list
+host_list = []
+
+# Generate the for loop to create a list
+i = 0
+for item in response_json["response"]:
+     i+=1
+     host = [
+             i,
+             item["hostType"],
+             item["hostIp"] 
+            ]
+     host_list.append( host )
+table_header = ["Number", "Type", "IP"]
+print( tabulate(host_list, table_header) )
+
