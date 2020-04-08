b242706fdcabaf1e9365e5cc66c8e0a68c1075a8
chunche95 <paeste95.pb@gmail.com>
2020-04-08 13:50:11 +0200

    Programación de redes - Devnet20 - Mod.3

---
+++ Model Driven Programmability - DevnNet20/Activities/Módulo 3/my_apic_em_functions2.py
@@ -0,0 +1,61 @@
+#!/bin/python
+import os
+os.system("clear")
+os.system("cls")
+
+import requests
+import json
+import tabulate
+
+
+from tabulate import *
+from my_apic_em_functions import *
+
+# Create the 'get_ticket' function
+def get_ticket():
+    api_url = "https://sandboxapicem.cisco.com/api/v1/ticket"
+    headers = {
+        "content-type": "application/json"
+    }
+    body_json = {
+        "username": "devnetuser",
+        "password": "Cisco123!"
+    }
+    # Send the request
+    resp=requests.post(api_url,json.dumps(body_json),headers=headers,verify=False)
+    #  Print the response and see the status 
+    status = resp.status_code
+    response_json = resp.json()
+    serviceTicket = response_json["response"]["serviceTicket"] 
+    print("______________________________________________________")
+    print("The service ticket number is: ", serviceTicket)
+    print("______________________________________________________")
+    return serviceTicket
+
+# Create the 'print_host' function
+def print_host():
+    api_url = "https://sandboxapicem.cisco.com/api/v1/host"
+    ticket = get_ticket()
+    headers = {
+    "content-type": "application/json",
+    "X-Auth-Token": ticket
+    }
+    # Request and handle errors.
+    resp = requests.get(api_url, headers=headers, verify=False)
+    response_json = resp.json()
+    # Parse and format the JSON response data
+    # Create a new list
+    host_list = []
+    # Generate the for loop to create a list
+    i = 0
+    for item in response_json["response"]:
+        i+=1
+        host = [
+                i,
+                item["hostType"],
+                item["hostIp"] 
+                ]
+        host_list.append( host )
+    table_header = ["Number", "Type", "IP"]
+    print( tabulate(host_list, table_header) )
+
