c018131f0a87c2295cf6e027e2cb5b73f0ec3cf3
chunche95 <paeste95.pb@gmail.com>
2020-04-22 19:36:59 +0200

    Others

--- Model Driven Programmability - DevnNet20/Files Scripts Python/01_hello-world_sol.py
+++ Model Driven Programmability - DevnNet20/Files Scripts Python/01_hello-world_sol.py
@@ -1,1 +1,2 @@
 print("Hello World!")
+print("My first Script in Python")
--- jupyterNotebook.py
+++ jupyterNotebook.py
@@ -1,3 +1,14 @@
-#!/bin/python
+#!/bin/bash
 
-jupyter notebook
+echo ("Python Request")
+ 
+python pip install --upgrade pip
+python pip install jupyter
+
+echo ("Jupyter location")
+where jupyter
+echo ("Python version")
+python -V
+
+echo ("Open Jupyter")
+/usr/bin/open -a "Firefox" 'http://localhost:8888'
