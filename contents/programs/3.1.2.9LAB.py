01aba001a3ec30d300ea2ab01ba5684cd38614cb
chunche95 <paeste95.pb@gmail.com>
2020-04-23 20:09:51 +0200

    Others

---
+++ contents/programs/3.1.2.9LAB.py
@@ -0,0 +1,20 @@
+#!/bin/python 
+import os
+os.system('clear')
+os.system('cls')
+# @author: [ Paulino Bermúdez R.]
+# @Description: Lab the break statement.
+
+print("""
+
+  ,                                         ___                                                   ___   __  
+/|   |                                    / (_)                               o                 /   \ /  \ 
+ |___|         _  _  _    __,   _  _     |      __   _  _  _     _        _|_     _  _    __,     __/|    |
+ |   |\|   |  / |/ |/ |  /  |  / |/ |    |     /  \_/ |/ |/ |  |/ \_|   |  |  |  / |/ |  /  |       \|    |
+ |   |/ \_/|_/  |  |  |_/\_/|_/  |  |_/   \___/\__/   |  |  |_/|__/  \_/|_/|_/|_/  |  |_/\_/|/  \___/o\__/ 
+                                                              /|                           /|              
+                                                              \|                           \|              
+""")
+while True:
+    word = input("Write a secret word: ")
+    if
