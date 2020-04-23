ca1d57ba80d0a95e3b4e0efda94a5d5471659d82
chunche95 <paeste95.pb@gmail.com>
2020-04-23 18:23:00 +0200

    Others

---
+++ contents/programs/Mississippi.py
@@ -0,0 +1,11 @@
+﻿import time
+
+# Write a for loop that counts to five.
+for i in range (1, 6):
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    print(i , " Mississippi \n")
+    # Body of the loop - use: time.sleep(1)
+    time.sleep(1)
+    
+# Write a print function with the final message.
+print(32 * "*","\n END looping for Mississippis")
