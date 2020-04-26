e2dbe0750378a8d4089cba4927a0acab491425ca
chunche95 <paeste95.pb@gmail.com>
2020-04-26 22:42:03 +0200

    Others

---
+++ contents/programs/3.1.2.14LAB.py
@@ -0,0 +1,13 @@
+blocks = int(input("Enter the number of blocks: "))
+
+# Calculate the sumatory
+# - I need the base, and resolve the next equation
+# blocks = n(n+1)/2 + height
+# 
+
+h=(n^2 + n - blocks)
+
+while blocks >
+
+
+print("The height of the pyramid:", height)
