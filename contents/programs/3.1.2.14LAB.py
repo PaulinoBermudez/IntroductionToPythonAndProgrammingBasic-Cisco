accda2c8ff114dfeb281cb2110f762dbd0f2958f
chunche95 <paeste95.pb@gmail.com>
2020-04-26 23:17:00 +0200

    Others

--- contents/programs/3.1.2.14LAB.py
+++ contents/programs/3.1.2.14LAB.py
@@ -1,13 +1,38 @@
 blocks = int(input("Enter the number of blocks: "))
 
-# Calculate the sumatory
-# - I need the base, and resolve the next equation
-# blocks = n(n+1)/2 + height
-# 
+# Tengo la siguiente situación:
+# Para tener: 
+#  1) altura          2) alturas          3) alturas            4) alturas          5) alturas
+# Necesito que:
+#                       *                       *                    *                      *       | 1
+#  *                   * *                     * *                  * *                    * *      | 2
+#                                             * * *                * * *                  * * *     | 3
+#                                                                 * * * *                * * * *    | 4
+#                                                                                       * * * * *   | 5
+# Los saltos son proporcionales porque son sumando 1 unidad.
+#           +2                  +3                      +4                      +5
+# Para saber qué número de bloques necesita cada altura es aplicando:
+#
+#  bloques = (h * (h + 1)) / 2
+#
+# Como el problema a resolver es el contrario, aplico la inversa de la operación:
+#
+# bloques * 2 = h ( h + 1) ==> Altura (H)
+# H => ((2*bloques) -h^2-h) = 0
+# H => h^2+h-(2*bloques)=0
+#
+# Descartamos los valores negativos de la operación cuadrática y tenemos la altura.
 
-h=(n^2 + n - blocks)
+# Inicializamos con la altura mínima que es 1 
+h=1
+eq=(h^2 + h - (2*blocks))
 
-while blocks >
+print("The height of the pyramid: "+ eq)
+while eq >= 0:
+    print("Vuelta nº:" + blocks )
+    blocks -= 1
+    eq -= 1
+
 
 
 print("The height of the pyramid:", height)
