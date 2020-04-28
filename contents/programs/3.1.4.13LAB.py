c7a43a68d43f65945342f1c2df5708bc7153f9ed
chunche95 <paeste95.pb@gmail.com>
2020-04-29 01:14:09 +0200

    Contents

--- contents/programs/3.1.4.13LAB.py
+++ contents/programs/3.1.4.13LAB.py
@@ -16,5 +16,14 @@ beatles.append("George Harrison")
 # Paso 3. Bucle FOR y append() para pedir al usuario que agregue a 
 # Stu Sutcliffe 
 # Pete Best.
+l = len(beatles)
+for i in range(l+1):
+    addList = input("Please, Add Stu and Pete in the Beatle List: ") 
+    beatles.append(addList)
 
-for i in range 
+# Paso 4. Eliminar a Stu y Pete de la lista
+l = len(beatles)
+for i in range(l):
+    print(beatles)
+    delList = input("Who delete of list? (Position) ")
+    del
