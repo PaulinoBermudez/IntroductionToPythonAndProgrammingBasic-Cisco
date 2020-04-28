e19c2aa7e4f4443149867e50842a9ada8d4d77ab
chunche95 <paeste95.pb@gmail.com>
2020-04-29 01:03:15 +0200

    Contents

---
+++ contents/programs/3.1.4.13LAB.py
@@ -0,0 +1,12 @@
+#!/bin/python 
+import os
+os.system('clear')
+os.system('cls')
+# @author: [ Paulino Bermúdez R.]
+# @Description: The Beatles List.
+
+# Paso 1. Crear lista
+beatles = []
+
+# Paso 2. Uso de append()
+beatles.append("Jonh Lennon")
