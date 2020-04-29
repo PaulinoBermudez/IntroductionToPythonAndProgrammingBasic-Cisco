51be4b76ebe0ad617d2376853210b2c6cb2b73b7
chunche95 <paeste95.pb@gmail.com>
2020-04-29 22:54:47 +0200

    Contents

---
+++ contents/programs/3.1.6.5-Slices.py
@@ -0,0 +1,17 @@
+﻿myList = [10, 8, 6, 4, 2]
+del myList[1:3]
+print(myList)
+
+print ("______________")
+
+myList = [10, 8, 6, 4, 2]
+del myList[:]
+print(myList)
+
+print ("______________")
+
+myList = [10, 8, 6, 4, 2]
+del myList
+print(myList) # El NameError es normal porque se ha borrado en el paso anterior.
+
+print ("______________")
