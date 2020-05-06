b1d8b1d12456f257f6a4df06918e620c06509c0a
chunche95 <paeste95.pb@gmail.com>
2020-05-06 20:02:34 +0200

    Contents

---
+++ contents/programs/3.1.6.7 Lists - More details.py
@@ -0,0 +1,25 @@
+﻿# Primera parte
+myList = [17, 3, 11, 5, 1, 9, 7, 15, 13]
+largest = myList[0]
+for i in myList:
+    if i > largest:
+        largest = i
+print(largest)
+print(5*"___")
+# Segunda parte -   parte de código.
+myList = [17, 3, 11, 5, 1, 9, 7, 15, 13]
+largest = myList[0]
+for i in range(1, len(myList)):
+    if myList[i] > largest:
+        largest = myList[i]
+print(largest)
+print(5*"___")
+
+# Tercera parte
+myList = [17, 3, 11, 5, 1, 9, 7, 15, 13]
+largest = myList[0]
+for i in myList[1:]:
+    if i > largest:
+        largest = i
+print(largest)
+print(5*"___")
---
+++ contents/programs/3.1.6.8 List-2.py
@@ -0,0 +1,9 @@
+﻿drawn = [5, 11, 9, 42, 3, 49]
+bets = [3, 7, 11, 42, 34, 49]
+hits = 0
+
+for number in bets:
+    if number in drawn:
+        hits += 1
+
+print(hits)
